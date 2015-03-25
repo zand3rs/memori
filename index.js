2deffd4fb72b8986819b2635d5816dc5b8be440a
zander <aamagtipon@voyagerinnovation.com>
2015-03-25 14:47:51 +0800

    Initial implementation

---
+++ index.js
@@ -0,0 +1,74 @@
+/*
+ * Memori
+ *
+ */
+
+var _ = require("lodash");
+var path = require("path");
+
+//==============================================================================
+//-- export
+
+module.exports = Memori;
+
+//==============================================================================
+//-- constructor
+
+function Memori(options) {
+  var _options = _.merge({adapter: "redis"}, options);
+  var _module = path.resolve(".", "lib", "adapters", _options.adapter);
+  var _adapter = require(_module);
+  this._client = new _adapter(_options);
+
+  Object.defineProperty(this, "client", {
+    get: function() {
+      return this._client;
+    }
+  });
+}
+
+//==============================================================================
+//-- public instance methods
+
+Memori.prototype.set = function(key, value, done) {
+  this.client.set(key, value, done);
+};
+
+//------------------------------------------------------------------------------
+//-- fake overloading, this is the real definition
+
+Memori.prototype.set = function(key, value, ttl, done) {
+  this.client.set(key, value, ttl, done);
+};
+
+//------------------------------------------------------------------------------
+
+Memori.prototype.get = function(key, done) {
+  this.client.get(key, done);
+};
+
+//------------------------------------------------------------------------------
+
+Memori.prototype.del = function(key, done) {
+  this.client.del(key, done);
+};
+
+//------------------------------------------------------------------------------
+
+Memori.prototype.incr = function(key, done) {
+  this.client.incr(key, done);
+};
+
+//------------------------------------------------------------------------------
+
+Memori.prototype.decr = function(key, done) {
+  this.client.decr(key, done);
+};
+
+//------------------------------------------------------------------------------
+
+Memori.prototype.keys = function(key, done) {
+  this.client.keys(key, done);
+};
+
+//==============================================================================
---
+++ test/unit/index.test.js
@@ -0,0 +1,197 @@
+var async = require("async"),
+    sinon = require("sinon"),
+    chai = require("chai"),
+    expect = chai.expect,
+    stub = sinon.stub,
+    Memori = require(process.cwd());
+
+describe("Memori", function() {
+
+  describe("constructor", function() {
+    it("accepts params", function() {
+      var cache = new Memori({
+        host: "test_host",
+        port: 9999,
+        database: "test_database",
+        username: "test_username",
+        password: "test_password",
+        ttl: 8888
+      });
+      expect(cache.client.host).to.equal("test_host");
+      expect(cache.client.port).to.equal(9999);
+      expect(cache.client.database).to.equal("test_database");
+      expect(cache.client.username).to.equal("test_username");
+      expect(cache.client.password).to.equal("test_password");
+      expect(cache.client.ttl).to.equal(8888);
+    });
+  });
+
+  describe("methods", function() {
+    before(function() {
+      cache = new Memori();
+    });
+
+    describe("#set", function() {
+      it("should store value into cache using the given key", function(done) {
+        cache.set("instance_key", "instance value", function(err, result) {
+          expect(err).to.not.exist;
+          expect(result).to.equal("OK");
+          done();
+        });
+      });
+      it("should store value into cache using the given key and ttl", function(done) {
+        cache.set("instance_key2", "instance value2", 300, function(err, result) {
+          expect(err).to.not.exist;
+          expect(result).to.equal("OK");
+          done();
+        });
+      });
+    });
+
+    describe("#get", function() {
+      it("should return value from cache using the given key", function(done) {
+        cache.get("instance_key", function(err, value) {
+          expect(err).to.not.exist;
+          expect(value).to.equal("instance value");
+          done();
+        });
+      });
+
+      it("should return values from cache using the given keys", function(done) {
+        cache.get(["instance_key", "instance_key2"], function(err, result) {
+          expect(err).to.not.exist;
+          expect(result).to.include("instance value");
+          expect(result).to.include("instance value2");
+          done();
+        });
+      });
+    });
+
+    describe("#del", function() {
+      before(function(done) {
+        async.series([
+          function(next) {
+            cache.set("key-01", "value 01", next);
+          },
+          function(next) {
+            cache.set("key-02", "value 02", next);
+          },
+          function(next) {
+            cache.set("key-03", "value 03", next);
+          }
+        ], done);
+      });
+
+      it("should delete value from cache using the given key", function(done) {
+        async.series([
+          function(next) {
+            cache.get(["key-01", "key-02", "key-03"], function(err, result) {
+              expect(err).to.not.exist;
+              expect(result).to.include("value 01");
+              expect(result).to.include("value 02");
+              expect(result).to.include("value 03");
+              next();
+            });
+          },
+          function(next) {
+            cache.del("key-01", function(err, result) {
+              expect(err).to.not.exist;
+              expect(result).to.equal(1);
+              next();
+            });
+          },
+          function(next) {
+            cache.del(["key-02", "key-03"], function(err, result) {
+              expect(err).to.not.exist;
+              expect(result).to.equal(2);
+              next();
+            });
+          }
+        ], function(err) {
+          cache.get(["key-01", "key-02", "key-03"], function(err, result) {
+            expect(err).to.not.exist;
+            expect(result).to.eql([null, null, null]);
+            done();
+          });
+        });
+      });
+    });
+
+    describe("#incr", function() {
+      before(function(done) {
+        cache.del("counter", done);
+      });
+
+      it("should increment value using the given key", function(done) {
+        async.series([
+          function(next) {
+            cache.incr("counter", function(err, value) {
+              expect(err).to.not.exist;
+              expect(value).to.equal(1);
+              next();
+            });
+          },
+          function(next) {
+            cache.incr("counter", function(err, value) {
+              expect(err).to.not.exist;
+              expect(value).to.equal(2);
+              next();
+            });
+          }
+        ], done);
+      });
+    });
+
+    describe("#decr", function() {
+      before(function(done) {
+        cache.del("counter", done);
+      });
+
+      it("should decrement value using the given key", function(done) {
+        async.series([
+          function(next) {
+            cache.decr("counter", function(err, value) {
+              expect(err).to.not.exist;
+              expect(value).to.equal(-1);
+              next();
+            });
+          },
+          function(next) {
+            cache.decr("counter", function(err, value) {
+              expect(err).to.not.exist;
+              expect(value).to.equal(-2);
+              next();
+            });
+          }
+        ], done);
+      });
+    });
+
+    describe("#keys", function() {
+      before(function(done) {
+        async.series([
+          function(next) {
+            cache.set("key-01", "value 01", next);
+          },
+          function(next) {
+            cache.set("key-02", "value 02", next);
+          },
+          function(next) {
+            cache.set("key-03", "value 03", next);
+          }
+        ], done);
+      });
+
+      it("should return cache keys using the given key", function(done) {
+        cache.keys("key-*", function(err, value) {
+          expect(err).to.not.exist;
+          expect(value).to.include("key-01");
+          expect(value).to.include("key-02");
+          expect(value).to.include("key-03");
+          done();
+        });
+      });
+    });
+  });
+
+});
