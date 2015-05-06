d1ea0d001b68ef770859ffd514ab9ce3d8b50026
zander <aamagtipon@voyagerinnovation.com>
2015-05-06 22:43:16 +0800

    added redis adapter tests

--- test/helpers/bootstrap.js
+++ test/helpers/bootstrap.js
@@ -4,3 +4,4 @@ var nodepath = require("path");
 global.async = require("async")
 global.Memori = require(cwd);
 global.Adapter = require(nodepath.join(cwd, "lib", "adapter"));
+global.Redis = require(nodepath.join(cwd, "lib", "adapters", "redis"));
