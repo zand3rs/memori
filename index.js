77f591df02d486134ab78396a7e4001210eab820
zander <aamagtipon@voyagerinnovation.com>
2016-03-08 20:45:12 +0800

    use a default noop callback for all methods

--- index.js
+++ index.js
@@ -39,62 +39,65 @@ function Memori(options) {
 //-- public instance methods
 
 Memori.prototype.set = function(key, value, done) {
-  this._adapter.set(key, value, done);
+  this._adapter.set(key, value, done || _.noop);
 };
 
 //------------------------------------------------------------------------------
 //-- fake overloading, this is the real definition
 
 Memori.prototype.set = function(key, value, ttl, done) {
-  this._adapter.set(key, value, ttl, done);
+  var _ttl = _.detect([ttl], _.isNumber);
+  var _done = _.detect([ttl, done, _.noop], _.isFunction);
+
+  this._adapter.set(key, value, _ttl, _done);
 };
 
 //------------------------------------------------------------------------------
 
 Memori.prototype.get = function(key, done) {
-  this._adapter.get(key, done);
+  this._adapter.get(key, done || _.noop);
 };
 
 //------------------------------------------------------------------------------
 
 Memori.prototype.del = function(key, done) {
-  this._adapter.del(key, done);
+  this._adapter.del(key, done || _.noop);
 };
 
 //------------------------------------------------------------------------------
 
 Memori.prototype.incr = function(key, done) {
-  this._adapter.incr(key, done);
+  this._adapter.incr(key, done || _.noop);
 };
 
 //------------------------------------------------------------------------------
 
 Memori.prototype.decr = function(key, done) {
-  this._adapter.decr(key, done);
+  this._adapter.decr(key, done || _.noop);
 };
 
 //------------------------------------------------------------------------------
 
 Memori.prototype.keys = function(pattern, done) {
-  this._adapter.keys(pattern, done);
+  this._adapter.keys(pattern, done || _.noop);
 };
 
 //------------------------------------------------------------------------------
 
 Memori.prototype.push = function(key, value, done) {
-  this._adapter.push(key, value, done);
+  this._adapter.push(key, value, done || _.noop);
 };
 
 //------------------------------------------------------------------------------
 
 Memori.prototype.pop = function(key, done) {
-  this._adapter.pop(key, done);
+  this._adapter.pop(key, done, _.noop);
 };
 
 //------------------------------------------------------------------------------
 
 Memori.prototype.expire = function(key, ttl, done) {
-  this._adapter.expire(key, ttl, done);
+  this._adapter.expire(key, ttl, done || _.noop);
 };
 
 //==============================================================================
